e63757d1b2618a73b92d158fbf553646d1bdc04e
AS <C:\Users\AS\Desktop\bb\TheBatPortable\TheBatPortable\Data\Mail>
2023-02-08 13:03:06 +0300

    Project Initialization

---
+++ app/src/main/java/com/example/authorization/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.authorization
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.Enter)
+        button.setOnClickListener {
+            val clogin:String = "1"
+            val cpassword:String = "1"
+            val nothing:String = ""
+            val login = findViewById<EditText>(R.id.Login)
+            val password = findViewById<EditText>(R.id.Password)
+            val check = findViewById<TextView>(R.id.Check)
+            if ((login.text.toString() == clogin) && (password.text.toString() == cpassword)){
+                check.setText("«Верно»")
+                check.setTextColor(Color.parseColor("#adff2f"))}
+            else{
+                login.setText(nothing)
+                password.setText(nothing)
+                check.setText("«Вы ошиблись в логине или пароле»")
+                check.setTextColor(Color.parseColor("#960018"))
+            }
+        }
+    }
+}
