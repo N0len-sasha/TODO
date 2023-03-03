b63cb9f9a554b9a724f0e6a5fc0f6f8e8ef2befc
N0len-sasha <sasaplatosin@gmail.com>
2023-03-03 21:17:20 +0300

    added visiable\un elements on Click

--- app/src/main/java/com/example/todo/ProfileActivity.kt
+++ app/src/main/java/com/example/todo/ProfileActivity.kt
@@ -2,11 +2,27 @@ package com.example.todo
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.todo.databinding.ProfileActivityBinding
+
 
 class ProfileActivity : AppCompatActivity(){
+    lateinit var binding: ProfileActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile_activity)
+        binding = ProfileActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.edit.setOnClickListener{
+            if (binding.confirm.visibility == View.GONE) {
+                binding.confirm.visibility = View.VISIBLE
+            }
+        }
+        binding.confirm.setOnClickListener {
+            if (binding.confirm.visibility == View.VISIBLE){
+                binding.confirm.visibility = View.GONE
+            }
+        }
+
     }
 }
