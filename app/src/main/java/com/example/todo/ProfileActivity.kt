eced7f08d315d605a687ee616d231e8f20c80116
N0len-sasha <sasaplatosin@gmail.com>
2023-03-03 19:59:20 +0300

    added profile activity XML with KT

---
+++ app/src/main/java/com/example/todo/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_activity)
+    }
+}
