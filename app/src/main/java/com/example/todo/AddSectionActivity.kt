430afc503158a762c3954d19d01362e512719e4d
N0len-sasha <sasaplatosin@gmail.com>
2023-03-04 16:55:06 +0300

    added addSectionActivity (not final)

---
+++ app/src/main/java/com/example/todo/AddSectionActivity.kt
@@ -0,0 +1,24 @@
+package com.example.todo
+
+import android.content.Intent
+import android.os.Binder
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todo.databinding.AddSectionActivityBinding
+import com.example.todo.databinding.SectionItemBinding
+
+lateinit var binding: AddSectionActivityBinding
+
+class AddSectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = AddSectionActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.done.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("name", binding.editName.text.toString())
+            startActivity(intent)
+        }
+    }
+}
