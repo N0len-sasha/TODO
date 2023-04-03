7551fc111a0fb0fed896494a4cb3b1bb6d61de66
N0len-sasha <sasaplatosin@gmail.com>
2023-04-03 15:12:07 +0300

    added authorization and registration functionality

--- app/src/main/java/com/example/todo/AuthorizationActivity.kt
+++ app/src/main/java/com/example/todo/AuthorizationActivity.kt
@@ -1,42 +1,62 @@
 package com.example.todo
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todo.databinding.AuthorizationActivityBinding
 import com.example.todo.databinding.RegistrationActivityBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthorizationActivity : AppCompatActivity() {
 
     private lateinit var binding: AuthorizationActivityBinding
+    private lateinit var auth: FirebaseAuth
 
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = AuthorizationActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
         binding.confirm.setOnClickListener {
-            val clogin: String = "1"
-            val cpassword: String = "1"
-            val nothing: String = ""
             val intent = Intent(this, MainActivity::class.java)
-            val login = binding.enterLogin
-            val password = binding.enterPassword
-            val check = binding.Check
-            if ((login.text.toString() == clogin) && (password.text.toString() == cpassword)) {
-                check.setText("«Верно»")
-                check.setTextColor(Color.parseColor("#adff2f"))
-                startActivity(intent)
-            } else {
-                login.setText(nothing)
-                password.setText(nothing)
-                check.setText("«Вы ошиблись в логине или пароле»")
-                check.setTextColor(Color.parseColor("#960018"))
+            val email: String = binding.enterLogin.text.toString().trim()
+            val password:String = binding.enterPassword.text.toString().trim()
+            if (TextUtils.isEmpty(email) || (email==null)){
+                Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show()
             }
+            if (TextUtils.isEmpty(password) || (email==null)){
+                Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show()
+            }
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(applicationContext, "Login Successful",
+                            Toast.LENGTH_SHORT).show()
+                            startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Authentication failed",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+
         }
         binding.registerNow.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
--- app/src/main/java/com/example/todo/RegistrationActivity.kt
+++ app/src/main/java/com/example/todo/RegistrationActivity.kt
@@ -1,21 +1,54 @@
 package com.example.todo
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todo.databinding.AuthorizationActivityBinding
 import com.example.todo.databinding.RegistrationActivityBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: RegistrationActivityBinding
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = RegistrationActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
         binding.confirm.setOnClickListener {
+            val email: String = binding.regLogin.text.toString().trim()
+            val password:String = binding.regPassword.text.toString().trim()
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, AuthorizationActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                    }
+            } else
+                Toast.makeText(this, "Empty fields are prohibited", Toast.LENGTH_SHORT).show()
+        }
+        binding.logIn.setOnClickListener {
             val intent = Intent(this, AuthorizationActivity::class.java)
             startActivity(intent)
         }
