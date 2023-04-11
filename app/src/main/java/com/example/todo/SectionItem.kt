e569e92c05856ce1b178f83e4b9c9d093a5aa516
N0len-sasha <sasaplatosin@gmail.com>
2023-04-11 15:31:33 +0300

    fixed edit but

---
+++ app/src/main/java/com/example/todo/SectionItem.kt
@@ -0,0 +1,25 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.todo.databinding.SectionItemBinding
+
+class SectionItem : Fragment(){
+    private lateinit var binding: SectionItemBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = SectionItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    fun edit() {
+        Log.e("error", "dds")
+        findNavController().navigate(R.id.action_mainFragment_to_addSectionFragment)
+    }
+}
